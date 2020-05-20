63360eca8b136f03f8d0d189dcac5a06de94d0a8
Ryan Johnson <ryan@258webco.com>
2020-05-19 19:44:42 -0500

    Retire this project, he's just to hard to understand.

--- client/src/components/AddArticle.js
+++ client/src/components/AddArticle.js
@@ -1,27 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 const AddArticle = () => {
+  const [title, setTitle] = useState("");
+  const [article, setArticle] = useState("");
+  const [authorName, setAuthorName] = useState("");
+
+  const changeOnClick = (e) => {
+    e.preventDefault();
+
+    const articles = { title, article, authorName };
+
+    axios
+      .post("/articles/add", articles)
+      .then((res) => console.log(res.data))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <AddArticleContainer>
       <div className="container">
         <h1>Add New Article</h1>
-        <form>
+        <form onSubmit={changeOnClick} encType="multipart/form-data">
           <div className="form-group">
             <label htmlFor="authorname">Author Name</label>
             <input
               type="text"
+              onChange={(e) => setAuthorName(e.target.value)}
               className="form-control"
               placeholder="Author Name"
             />
           </div>
           <div className="form-group">
             <label htmlFor="title">Title</label>
-            <input type="text" className="form-control" placeholder="Title" />
+            <input
+              type="text"
+              onChange={(e) => setTitle(e.target.value)}
+              className="form-control"
+              placeholder="Title"
+            />
           </div>
           <div class="form-group">
             <label htmlFor="article">Article</label>
-            <textarea class="form-control" rows="3"></textarea>
+            <textarea
+              class="form-control"
+              rows="3"
+              onChange={(e) => setArticle(e.target.value)}
+            ></textarea>
           </div>
           <button type="submit" className="btn btn-primary">
             Post Article
